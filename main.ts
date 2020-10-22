3374c588c6b017e9385cc9ccca67bf4d425b0923
BroccoliSurprise <61516592+BroccoliSurprise@users.noreply.github.com>
2020-10-22 23:23:17 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,6 +2,7 @@ input.onButtonPressed(Button.A, function () {
     if (stemmeUrneÅpen == true) {
         radio.sendString("ja")
         stemmeUrneÅpen = false
+        mhm_ok("J")
         basic.showLeds(`
             . . # . .
             . . # . .
@@ -11,6 +12,9 @@ input.onButtonPressed(Button.A, function () {
             `)
     }
 })
+function mhm_ok (text: string) {
+    list.push(text)
+}
 /**
  * Dette er programmet til mentometer-senderen!
  * 
@@ -21,12 +25,16 @@ input.onButtonPressed(Button.A, function () {
  * Dette styres ved at micro:biten kun kjører kommandoene på A og B-knappene når "valgUrneÅpen"-variabelen er TRUE (når microbiten mottar "start"-signalet fra en annen micro:bit.)
  * 
  * Etter at stemme har blitt avgitt, settes denne tilbake til FALSE.
+ * 
+ * .
+ * 
+ * ... og noen ser ut til å ha programmert inn en sårbarhet i stemmegivningen.
  */
 radio.onReceivedString(function (receivedString) {
     if (receivedString == "start") {
         stemmeUrneÅpen = true
         basic.showString("?")
-    } else if (receivedString == "stopp") {
+    } else if (receivedString == "stopp" && stemmeUrneÅpen == true) {
         stemmeUrneÅpen = false
         basic.showLeds(`
             . . # . .
@@ -35,12 +43,16 @@ radio.onReceivedString(function (receivedString) {
             . . . . .
             . . # . .
             `)
+        mhm_ok("?")
+    } else if (receivedString == "hack") {
+        hack()
     }
 })
 input.onButtonPressed(Button.B, function () {
     if (stemmeUrneÅpen == true) {
         radio.sendString("nei")
         stemmeUrneÅpen = false
+        mhm_ok("N")
         basic.showLeds(`
             . . # . .
             . . # . .
@@ -50,7 +62,17 @@ input.onButtonPressed(Button.B, function () {
             `)
     }
 })
+function hack () {
+    for (let index = 0; index <= list.length - 1; index++) {
+        basic.showString("" + convertToText(index + 1) + "=" + list[index])
+        basic.clearScreen()
+        basic.pause(400)
+    }
+}
+let list: string[] = []
 let stemmeUrneÅpen = false
 radio.setGroup(1)
 basic.showIcon(IconNames.Happy)
 stemmeUrneÅpen = false
+// Denne blokken ser helt uskyldig ut, ikke sant?
+list = []
